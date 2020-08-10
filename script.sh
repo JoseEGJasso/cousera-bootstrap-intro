9ff41a67d5ceb90bac851fed75fa32f46d03dfac
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2020-08-09 21:23:06 -0500

    Agrego scripts para la puesta en produccion

---
+++ script.sh
@@ -0,0 +1,8 @@
+usemin index.html -d dist --htmlmin -o dist/index.html
+usemin ./src/almacenamiento.html -d dist --htmlmin -o dist/almacenamiento.html
+usemin ./src/componentes.html -d dist --htmlmin -o dist/componentes.html
+usemin ./src/contacto.html -d dist --htmlmin -o dist/contacto.html
+usemin ./src/mem_ram.html -d dist --htmlmin -o dist/mem_ram.html
+usemin ./src/motherboards.html -d dist --htmlmin -o dist/motherboards.html
+usemin ./src/procesadores.html -d dist --htmlmin -o dist/procesadores.html
+usemin ./src/tarjetas_video.html -d dist --htmlmin -o dist/tarjetas_video.html
